eb42371bc0dc197109696f32343ca987ba1ac0bd
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-02-22 15:47:18 +0000

    Added comment model

--- blog/migrations/0002_post_author_post_content_post_created_on_and_more.py
+++ blog/migrations/0002_post_author_post_content_post_created_on_and_more.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='post',
             name='author',
-            field=models.ForeignKey(default='Anonymous', on_delete=django.db.models.deletion.CASCADE, related_name='blog_posts', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='blog_posts', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
         migrations.AddField(

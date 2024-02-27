9c6fcc759e98f1cbcc5aa1d7b012e47e377e5bd0
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-02-27 12:25:48 +0000

    Enabled serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -43,8 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
-    'about',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -52,6 +51,9 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
+    'blog',
+    'about',
     ]
 
 SITE_ID = 1
